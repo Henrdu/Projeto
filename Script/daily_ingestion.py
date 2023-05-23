4744da5457088b1554c5986f4194f3b9551df0c8
Henrdu <henriqueoliveiradias.177@gmail.com>
2023-05-23 09:35:40 -0300

    atualizar chave

--- Script/daily_ingestion.py
+++ Script/daily_ingestion.py
@@ -8,19 +8,21 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 # ADA - qzawljRxB5bYu
 # BRL - n5fpnvMGNsOS
 
+
 def get_date_list():
     date_list = []
-    for i in range(0,10):
-        data =  (datetime.now() - timedelta(days=i)).date()
+    for i in range(0, 10):
+        data = (datetime.now() - timedelta(days=i)).date()
         date_list.append(data)
     return date_list
+
 
 def get_unixtimestamp_dict():
     unix_timestamp_dict = {}
     date_list = get_date_list()
     for date in date_list:
         date_hour = str(date) + ' 21:00:00'
-        formated_date = datetime.strptime(date_hour,"%Y-%m-%d %H:%M:%S")
+        formated_date = datetime.strptime(date_hour, "%Y-%m-%d %H:%M:%S")
         unix_timestamp = datetime.timestamp(formated_date)
         unix_timestamp_dict[str(date)] = unix_timestamp
     return unix_timestamp_dict
@@ -28,12 +30,14 @@ def get_unixtimestamp_dict():
 
 def api_request():
     unix_timestamp_dict = get_unixtimestamp_dict()
-    currencies = {'btc':'Qwsogvtv82FCd', 'eth': 'razxDUgYGNAdQ', 'ada':'qzawljRxB5bYu'}
+    currencies = {'btc': 'Qwsogvtv82FCd',
+                  'eth': 'razxDUgYGNAdQ', 'ada': 'qzawljRxB5bYu'}
     for currency in list(currencies.keys()):
         for date in list(unix_timestamp_dict.keys()):
             url = f"https://coinranking1.p.rapidapi.com/coin/{currencies[currency]}/price"
 
-            querystring = {"referenceCurrencyUuid":"n5fpnvMGNsOS","timestamp":f"{unix_timestamp_dict[date]}"}
+            querystring = {"referenceCurrencyUuid": "n5fpnvMGNsOS",
+                           "timestamp": f"{unix_timestamp_dict[date]}"}
 
             headers = {
                 "X-RapidAPI-Key": "85ecd427femsh9cd687b4015085bp1aa3d2jsn146974367318",
@@ -50,8 +54,9 @@ def generate_path(currency, date):
     path = f'daily/raw/{currency}/{date[0:4]}/{date[5:7]}/{date[8:10]}/moeda.json'
     return path
 
+
 def save_json_azure(path, json_file):
-    connect_str = "DefaultEndpointsProtocol=https;AccountName=aulafiaead;AccountKey=T4tkZipg4JNvC9d2X0OImaCi8HFq3tgb+i8YXk2i4UHx8iuoC6PwdPWrp3GXYkj+VFjbhal9PYuG+AStdF6M0g==;EndpointSuffix=core.windows.net"
+    connect_str = "xxxxxxx"
     container_name = "grupo1"
     blob_name = path
 
@@ -64,4 +69,3 @@ def save_json_azure(path, json_file):
 
 if __name__ == '__main__':
     api_request()
-    
